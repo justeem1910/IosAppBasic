57670d1e97212565c3640008a7105377d9f66cc3
Hoang Long <dohoanglong8@gmail.com>
2022-06-20 00:00:41 +0700

    Otp 70%

--- App_Intern_basic/ViewController/OtpViewController.swift
+++ App_Intern_basic/ViewController/OtpViewController.swift
@@ -18,10 +18,14 @@ class OtpViewController: UIViewController {
     @IBOutlet weak var tfOtp4:UITextField!
     @IBOutlet weak var tfOtp5:UITextField!
     @IBOutlet weak var tfOtp6:UITextField!
-    //aaaaadsfasdf
-    //aaaaa
+
+    
+    
     var stringNumber = "Vui lòng nhập mã gồm 4 chữ số đã được gửi đến bạn vào số điện thoại "
     var phoneNumber = ""
+    override func viewDidLayoutSubviews() {
+    
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         setView()
@@ -32,8 +36,9 @@ class OtpViewController: UIViewController {
         tfOtp4.delegate = self
         tfOtp5.delegate = self
         tfOtp6.delegate = self
+        
     
-    
+        
     }
     func setView(){
         let numberAttribute = [ NSAttributedString.Key.font: UIFont(name: "NunitoSans-Regular", size: 14) ]
@@ -45,6 +50,67 @@ class OtpViewController: UIViewController {
         let phoneAttString = NSAttributedString(string: phoneNumber, attributes: phoneAttribute as [NSAttributedString.Key : Any])
         numberAttString.append(phoneAttString)
         lblPhone.attributedText = numberAttString
+        
+        
+        
+        setTextFieldOtp(textField: tfOtp1)
+        setTextFieldOtp(textField: tfOtp2)
+        setTextFieldOtp(textField: tfOtp3)
+        setTextFieldOtp(textField: tfOtp4)
+        setTextFieldOtp(textField: tfOtp5)
+        setTextFieldOtp(textField: tfOtp6)
+        
+        tfOtp1.addTarget(self, action: #selector(self.OtpTapAction), for: .editingChanged)
+        tfOtp2.addTarget(self, action: #selector(self.OtpTapAction), for: .editingChanged)
+        tfOtp3.addTarget(self, action: #selector(self.OtpTapAction), for: .editingChanged)
+        tfOtp4.addTarget(self, action: #selector(self.OtpTapAction), for: .editingChanged)
+        tfOtp5.addTarget(self, action: #selector(self.OtpTapAction), for: .editingChanged)
+        tfOtp6.addTarget(self, action: #selector(self.OtpTapAction), for: .editingChanged)
+        
+        
+    }
+    @objc func OtpTapAction (textField:UITextField){
+        if textField.text?.count ?? 0 == 1 {
+            switch textField{
+            case tfOtp1:
+                checkOtpTfEmpty()
+            case tfOtp2:
+                checkOtpTfEmpty()
+            case tfOtp3:
+                checkOtpTfEmpty()
+            case tfOtp4:
+                checkOtpTfEmpty()
+            case tfOtp5:
+                checkOtpTfEmpty()
+            case tfOtp6:
+               checkOtpTfEmpty()
+            default:
+                break
+            }
+        }
+    }
+    func checkOtpTfEmpty(){
+        if tfOtp1.text?.count == 0 {
+            tfOtp1.becomeFirstResponder()
+        } else if tfOtp2.text?.count == 0 {
+            tfOtp2.becomeFirstResponder()
+        } else if tfOtp3.text?.count == 0 {
+            tfOtp3.becomeFirstResponder()
+        } else if tfOtp4.text?.count == 0 {
+            tfOtp4.becomeFirstResponder()
+        } else if tfOtp5.text?.count == 0 {
+            tfOtp5.becomeFirstResponder()
+        } else if tfOtp6.text?.count == 0 {
+            tfOtp6.becomeFirstResponder()
+        }
+    }
+    
+    func setTextFieldOtp (textField :UITextField){
+        textField.layer.cornerRadius = 8
+        textField.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.08).cgColor
+        textField.layer.shadowOpacity = 1
+        textField.layer.shadowRadius = 8
+        textField.layer.shadowOffset = CGSize(width: 0, height: 4)
     }
     
     
@@ -52,7 +118,16 @@ class OtpViewController: UIViewController {
     @IBAction func btnBack(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
+
 }
 extension OtpViewController: UITextFieldDelegate{
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField.text!.count == 1 && !string.isEmpty{
+            return false
+        } else {
+            return true
+        }
     
+    }
 }
+
